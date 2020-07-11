f2fd58a8376dce04456e82f835606abdbce3818e
caoquerque <caoquerque@gmail.com>
2020-07-11 17:32:30 -0300

    Has_many added to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,14 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  attr_writer :login
+  
+  has_many :to_do_lists, dependent: :destroy
+  has_many :favourite_list, through: :to_do_lists, dependent: :destroy
+  has_many :tasks, through: :to_do_lists
+
+
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, :authentication_keys => [:login]
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,33 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_10_145028) do
+ActiveRecord::Schema.define(version: 2020_07_11_164108) do
+
+  create_table "favourite_lists", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "to_do_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["to_do_list_id"], name: "index_favourite_lists_on_to_do_list_id"
+    t.index ["user_id"], name: "index_favourite_lists_on_user_id"
+  end
+
+  create_table "tasks", force: :cascade do |t|
+    t.string "description"
+    t.boolean "done"
+    t.integer "to_do_list_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["to_do_list_id"], name: "index_tasks_on_to_do_list_id"
+  end
+
+  create_table "to_do_lists", force: :cascade do |t|
+    t.boolean "public"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_to_do_lists_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -20,8 +46,14 @@ ActiveRecord::Schema.define(version: 2020_07_10_145028) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "favourite_lists", "to_do_lists"
+  add_foreign_key "favourite_lists", "users"
+  add_foreign_key "tasks", "to_do_lists"
+  add_foreign_key "to_do_lists", "users"
 end
